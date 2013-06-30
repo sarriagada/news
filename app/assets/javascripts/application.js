b29eeab8b3ab10556980ba5689a7d7bbafabe336
Sergio Sebastian Arriagada <sarriagada87@gmail.com>
2013-06-30 04:09:46 -0300

    se modifico el idioma de redactor

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,7 +16,6 @@
 //= require bootstrap
 //= require redactor-rails/redactor.min
 //= require redactor-rails/config.js
-//= require redactor-rails/langs/es.js
 //= require dataTables/jquery.dataTables
 //= require dataTables/jquery.dataTables.bootstrap
 //= require notes
